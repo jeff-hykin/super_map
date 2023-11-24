6f45449c7b90ea4f03478cd6203df4e505cd3653
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-24 17:57:21 -0600

    -

--- main/super_map/__init__.py
+++ main/super_map/__init__.py
@@ -318,8 +318,6 @@ class type(real_type):
     def __new__(cls, *args, **kwargs):
         normal_output = real_type(*args, **kwargs)
         return type_mappings.get(normal_output, normal_output)
-for each_key, each_value in real_type.__dict__.items():
-    type.__dict__[each_key] = each_value
 for each in dir(real_type):
     try:
         setattr(type, getattr(real_type, each))
