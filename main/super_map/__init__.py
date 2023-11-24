3d2a2c6777aed9a4cd9738aabbbb37d6ded03c3d
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-24 15:12:20 -0600

    improve type hack

--- main/super_map/__init__.py
+++ main/super_map/__init__.py
@@ -314,6 +314,7 @@ isinstance.__doc__ = real_isinstance.__doc__
 builtins.isinstance = isinstance
 real_type = builtins.type
 class type(real_type):
+    __dict__ = real_type.__dict__["__dict__"]
     def __new__(cls, *args, **kwargs):
         normal_output = real_type(*args, **kwargs)
         return type_mappings.get(normal_output, normal_output)
@@ -321,7 +322,7 @@ for each_key, each_value in real_type.__dict__.items():
     type.__dict__[each_key] = each_value
 for each in dir(real_type):
     try:
-        setattr(type,  r(real_type, each))
+        setattr(type, getattr(real_type, each))
     except Exception as error:
         pass
 builtins.type = type
